4b00c24dbbfe610227b613ac50af07d0d113ddf7
ZainF24 <165853011+ZainF24@users.noreply.github.com>
2024-06-16 23:22:18 -0500

    Setup

---
+++ script.js
@@ -0,0 +1,15 @@
+// Write logic to get computer choice
+function getComputerChoice() {
+    const options = ["rock", "paper", "scizzors"];
+    return options = options[Math.floor(Math.random() * options.length)]
+    }
+    // Test function
+    // Write the logic to get human choice
+    function getHumanChoice() {
+        const choice = prompt("Rock, Paper, or Scizzors?");
+
+    }
+    // Declare player score variables
+    
+    // Write the logic to play a single round
+    // Write the logic top play 5 rounds 
