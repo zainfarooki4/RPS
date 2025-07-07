74531274387695ee1aa128b007f7c70df9beae01
ZainF24 <165853011+ZainF24@users.noreply.github.com>
2025-07-07 10:10:47 -0500

    Game is complete.

--- script.js
+++ script.js
@@ -13,15 +13,36 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+function playGame() {
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-        console.log("Tie!");
-    }
-    // return "You chose: " + humanChoice + "." + " Computer chose: " + computerChoice;
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            console.log("Tie!");
+        } else if (
+            (humanChoice === "rock" && computerChoice === "scissors") || 
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")
+        ) { 
+            console.log("You win!");
+            humanScore++;
+        } else {
+            console.log("You lose!");
+            computerScore++;
+        }
+        return "Your score: " + humanScore + "." + " Computer score " + computerScore;
     
 
-}
+    }
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 console.log(playRound(humanSelection, computerSelection));
+
+}
+for (let i = 1; i <= 5; i++) {
+    playGame();
+}
+if (computerScore > humanScore) {
+    console.log("Computer wins the game!");
+} else {
+    console.log("You win the game!");
+}
